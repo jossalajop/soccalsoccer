cfdf1b469927192f0106bdead456f875092d0327
unknown <dil.alcivar@yavirac.edu.ec>
2023-08-18 10:10:32 -0500

    se agrega estilos login, register

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 const MYSQLHOST = process.env.MYSQLHOST || 'localhost'
 const MYSQLUSER = process.env.MYSQLUSER || 'root'
 const MYSQLPASSWORD = process.env.MYSQLPASSWORD || ''
-const MYSQLDATABASE = process.env.MYSQLDATABASE || 'social'
+const MYSQLDATABASE = process.env.MYSQLDATABASE || 'social1'
 const MYSQLPORT = process.env.MYSQLPORT || 3306
 const MYSQL_URI = process.env.MYSQL_URI || ''
 
